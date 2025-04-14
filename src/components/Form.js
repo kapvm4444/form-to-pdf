9252607ef5014654d0eaeeb26434eb7239a03996
Khush <kapvm4444@gmail.com>
2025-04-14 15:00:53 +0530

    fixed dark mode

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,87 +1,137 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import TextField from "./TextField";
 import Dropdown from "./Dropdown";
-import {documentize} from "./hooks/documentize";
+import { documentize } from "./hooks/documentize";
 import Button from "./Button";
 
-const genders = ['Male', 'Female', 'Other']
-const diagnosisValues = ['A', 'B', 'C']
-const treatmentValues = ['A', 'B', 'C']
+const genders = ["Male", "Female", "Other"];
+const diagnosisValues = ["A", "B", "C"];
+const treatmentValues = ["A", "B", "C"];
 
 export default function Form() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("Male");
+  const [mobile, setMobile] = useState("+91 ");
+  const [village, setVillage] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [diagnosis, setDiagnosis] = useState(diagnosisValues[0]);
+  const [treatment, setTreatment] = useState(treatmentValues[0]);
+  const [medicine, setMedicine] = useState("");
 
-    const [name, setName] = useState("")
-    const [age, setAge] = useState("")
-    const [gender, setGender] = useState('Male')
-    const [mobile, setMobile] = useState('+91')
-    const [village, setVillage] = useState('')
-    const [city, setCity] = useState('')
-    const [state, setState] = useState('')
-    const [diagnosis, setDiagnosis] = useState('')
-    const [treatment, setTreatment] = useState('')
-    const [medicine, setMedicine] = useState('')
+  function handleSubmit(print) {
+    documentize({
+      name,
+      age,
+      gender,
+      mobile,
+      village,
+      city,
+      state,
+      diagnosis,
+      treatment,
+      medicine,
+    });
+  }
 
-    function handleSubmit(print){
-        documentize({
-            name, age, gender, mobile, village, city, state, diagnosis, treatment, medicine
-        })
-    }
+  function handleClear() {
+    setName("");
+    setAge("");
+    setGender("Male");
+    setMobile("+91 ");
+    setVillage("");
+    setCity("");
+    setState("");
+    setDiagnosis("");
+    setTreatment("");
+    setMedicine("");
+  }
 
+  return (
+    <form
+      className="max-w-sm mx-auto mt-8"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      {/*Patient Name Field*/}
+      <TextField
+        placeholder={"i.e. John Doe"}
+        value={name}
+        onChange={(el) => setName(el.target.value)}
+      >
+        Patient Name
+      </TextField>
 
-    return (
-        <form className="max-w-sm mx-auto mt-8" onSubmit={(e => e.preventDefault())}>
+      {/*Age*/}
+      <TextField value={age} onChange={(el) => setAge(el.target.value)}>
+        Age
+      </TextField>
 
-            {/*Patient Name Field*/}
-            <TextField placeholder={"i.e. John Doe"} value={name} onChange={el => setName(el.target.value)}>
-                Patient Name
-            </TextField>
+      {/*Gender*/}
+      <Dropdown
+        options={genders}
+        value={gender}
+        onChange={(el) => setGender(el.target.value)}
+      >
+        Gender
+      </Dropdown>
 
-            {/*Age*/}
-            <TextField value={age} onChange={el => setAge(el.target.value)}>
-                Age
-            </TextField>
+      {/*Mobile*/}
+      <TextField
+        placeholder={"+91 "}
+        value={mobile}
+        onChange={(el) => setMobile(el.target.value)}
+      >
+        Mobile
+      </TextField>
 
-            {/*Gender*/}
-            <Dropdown options={genders} value={gender} onChange={el => setGender(el.target.value)}>Gender</Dropdown>
+      {/*Village*/}
+      <TextField value={village} onChange={(el) => setVillage(el.target.value)}>
+        Village
+      </TextField>
 
-            {/*Mobile*/}
-            <TextField placeholder={"+91 "} value={mobile} onChange={el => setMobile(el.target.value)}>
-                Mobile
-            </TextField>
+      {/*City*/}
+      <TextField value={city} onChange={(el) => setCity(el.target.value)}>
+        City
+      </TextField>
 
-            {/*Village*/}
-            <TextField value={village} onChange={el => setVillage(el.target.value)}>
-                Village
-            </TextField>
+      {/*State*/}
+      <TextField value={state} onChange={(el) => setState(el.target.value)}>
+        State
+      </TextField>
 
-            {/*City*/}
-            <TextField value={city} onChange={el => setCity(el.target.value)}>
-                City
-            </TextField>
+      {/*Diagnosis*/}
+      <Dropdown
+        options={diagnosisValues}
+        value={diagnosis}
+        onChange={(el) => setDiagnosis(el.target.value)}
+      >
+        Diagnosis
+      </Dropdown>
 
-            {/*State*/}
-            <TextField value={state} onChange={el => setState(el.target.value)}>
-                State
-            </TextField>
+      {/*Treatment*/}
+      <Dropdown
+        options={treatmentValues}
+        value={treatment}
+        onChange={(el) => setTreatment(el.target.value)}
+      >
+        Treatment
+      </Dropdown>
 
-            {/*Diagnosis*/}
-            <Dropdown options={diagnosisValues} value={diagnosis}
-                      onChange={el => setDiagnosis(el.target.value)}>Diagnosis</Dropdown>
+      {/*Medicine*/}
+      <TextField
+        type="textarea"
+        value={medicine}
+        onChange={(el) => setMedicine(el.target.value)}
+      >
+        Medicine
+      </TextField>
 
-            {/*Treatment*/}
-            <Dropdown options={treatmentValues} value={treatment}
-                      onChange={el => setTreatment(el.target.value)}>Treatment</Dropdown>
-
-            {/*Medicine*/}
-            <TextField type='textarea' value={medicine} onChange={el => setMedicine(el.target.value)}>
-                Medicine
-            </TextField>
-
-            <div className='flex justify-evenly'>
-                <Button onClick={() => handleSubmit()}>Save</Button>
-                <Button onClick={() => handleSubmit(true)}>Print</Button>
-            </div>
-
-        </form>
-    );
+      <div className="flex justify-evenly">
+        <Button onClick={() => handleSubmit()}>Save</Button>
+        <Button onClick={() => handleSubmit(true)}>Print</Button>
+        <Button onClick={() => handleClear()}>Clear</Button>
+      </div>
+    </form>
+  );
 }
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React from "react";
 
-export default function TextField({children, placeholder, type = 'text', value, onChange}) {
-    return (
-        <>
-            <div className="mb-5">
-                <label className="block mb-2  text-gray-900 dark:text-white">
-                    {children}
-                </label>
-                {type === 'textarea' ?
-                    <textarea id="message" rows="4" placeholder={placeholder} value={value} onChange={onChange}
-                              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
-                    :
-                    <input type={type} id="name" placeholder={placeholder} value={value} onChange={onChange}
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       required/>
-                }
-
-            </div>
-        </>
-    );
+export default function TextField({
+  children,
+  placeholder,
+  type = "text",
+  value,
+  onChange,
+}) {
+  return (
+    <>
+      <div className="mb-5">
+        <label className="block mb-2 text-white">{children}</label>
+        {type === "textarea" ? (
+          <textarea
+            id="message"
+            rows="4"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className="block p-2.5 w-full text-sm rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+          ></textarea>
+        ) : (
+          <input
+            type={type}
+            id="name"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className="border text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white"
+            required
+          />
+        )}
+      </div>
+    </>
+  );
 }
-
